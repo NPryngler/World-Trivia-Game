e48992b401e397370e3160e66a21402bc6f5684c
Natasha Pryngler <natasha.pryngler@gmail.com>
2018-08-19 17:57:24 -0400

    included asian questions

--- main.js
+++ main.js
@@ -71,12 +71,6 @@ for (let i = 0; i < continentOptions.length; i++) {
 
 
 
-let randomQuestionIndex = 0;
-let currentQuestion = {};
-let possibleAnswers = [];
-let round = 1;
-let score = 0;
-
 function europe() {
   let europeQuestions = [
     { question: 'If you were to visit the town of Sintra, which country would you go?', answer: 'Portugal' },
@@ -86,7 +80,7 @@ function europe() {
     { question: 'You will visit a country famous for its banks and watches? Which country will you go?', answer: 'Switzerland' },
     { question: 'You will taste the original Budweiser Budvar in its brewery. Which country will you go?', answer: 'Czech Republic' },
     { question: 'You may be able to see for yourself Mr. Dracula in Transylvania. Which country will you go?', answer: 'Romania' },
-    { question: 'Why dont you visit the largest city in Europe – that spans to Asia too. Where should you go?', answer: 'Turkey' },
+    { question: 'You can visit the pope\'s palace that was not in Rome. Where should you go?', answer: 'France' },
     { question: 'You can go to the birthplace of french-fries and have good beer too. Which country will you go?', answer: 'Belgium' },
     { question: 'You can get to see the remains of the most famous wall of the modern age. Where will you go?', answer: 'Germany' },
     { question: 'You can visit the place where the tie was invented (maybe to regret the fact).Where would you go?', answer: 'Croatia' },
@@ -95,7 +89,13 @@ function europe() {
     { question: 'You can visit the country that is the home of the world\'s most famous queen. Where will you go?', answer: 'United Kindgom' },
   ];
 
-  const europeanCountries = ['Austria', 'Belgium', 'Bulgaria', 'Croacia', 'Cyprus', 'Czech Republic', 'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary', 'Ireland', 'Italy', 'Latvia', 'Lithuania', 'Luxembourg', 'Malta', 'The Netherlands', 'Poland', 'Portugal', 'Romania', 'Slovakia', 'Slovenia', 'Spain', 'Sweden', 'United Kingdom', 'United Kingdom', 'Turkey', 'Ukraine'];
+  const europeanCountries = ['Austria', 'Belgium', 'Bulgaria', 'Croacia', 'Cyprus', 'Czech Republic', 'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary', 'Ireland', 'Italy', 'Latvia', 'Lithuania', 'Luxembourg', 'Malta', 'The Netherlands', 'Poland', 'Portugal', 'Romania', 'Slovakia', 'Slovenia', 'Spain', 'Sweden', 'United Kingdom', 'United Kingdom', 'Ukraine'];
+
+  let randomQuestionIndex = 0;
+  let currentQuestion = {};
+  let possibleAnswers = [];
+  let round = 1;
+  let score = 0;
 
   function stylePage() {
     for (let i = 0; i < countryOptionsElements.length; i++) {
@@ -239,19 +239,25 @@ function america() {
     { question: 'If you were to visit the land of samba music and Copacabana beach, which country would you go?', answer: 'Brazil' },
     { question: 'You can visit the Statue of Liberty and watch world famous musicals at Broadway, which country should you be?', answer: 'United States' },
     { question: 'You will taste exquisite tequila where it was invented and visit old Mayan pyramides. Which country will you land?', answer: 'Mexico' },
-    { question: 'You can visit the cities of Medellin and Cali, most famous for its past drug cartels and feared drug lords. Where will you go', answer: 'Colombia' },
+    { question: 'You can visit the cities of Medellin and Cali, most famous for its past drug cartels and feared drug lords. Where will you go?', answer: 'Colombia' },
     { question: 'You can dine a tasty barbecue while watching first class tango dancers. Which country should you be?', answer: 'Argentina' },
     { question: 'You can visit the second largest country of the world. A country that has more lakes than the rest of the world combined. Which country will you go?', answer: 'Canada' },
     { question: 'You will be able to visit Machu Pichu. Which country will you go?', answer: 'Peru' },
-    { question: 'You can learn some words in Guarany - an official language of this country along with spanish. Where should you go?', answer: 'Paraguay' },
+    { question: 'You can learn some words in Guarany - an official language of this country along with spanish - and ride an Alpaca. Where should you go?', answer: 'Paraguay' },
     { question: 'You can stay at an hotel made almost entirely of salt at the edge of the Salar the Uyuni plain. Which country will you go?', answer: 'Bolivia' },
     { question: 'You can drink rum and smoke artisanal cigars at the largest island of the Caribbean. Where will you go?', answer: 'Cuba' },
-    { question: 'You can visit the Atacama Desert, the Andes Mountains and Patagonia without living this country. Where would you go?', answer: 'Chile' },
+    { question: 'You can visit the Atacama Desert, the Andes Mountains and Patagonia without leaving this country. Where would you go?', answer: 'Chile' },
     { question: 'You can visit it\'s capital, Santo Domingo, the oldest European settlement in the Americas founded in 1496. Where will you go?', answer: 'Dominican Republic' },
     { question: 'You can get to know the birthplace of reggae and Bob Marley. Where will you go?', answer: 'Jamaica' },
     { question: 'You will meet people - also known as Ticos - from one of the happiest countries of the world. Where will you go?', answer: 'Costa Rica' },
   ];
   const americanCountries = ['United States', 'Brazil', 'Mexico', 'Colombia', 'Argentina', 'Canada', 'Peru', 'Venezuela', 'Chile', 'Ecuador', 'Guatemala', 'Cuba', 'Bolivia', 'Haiti', 'Dominican Republic', 'Honduras', 'Paraguay', 'Nicaragua', 'El Salvador', 'Costa Rica', 'Panama', 'Uruguay', 'Jamaica', 'Trinidad and Tobago', 'Guyana', 'Suriname', 'Guadeloupe', 'Martinique', 'Bahamas', 'Belize', 'Barbados'];
+
+  let randomQuestionIndex = 0;
+  let currentQuestion = {};
+  let possibleAnswers = [];
+  let round = 1;
+  let score = 0;
 
   function stylePage() {
     for (let i = 0; i < countryOptionsElements.length; i++) {
@@ -336,9 +342,7 @@ function america() {
       setTimeout(clearPage, 2000);
 
     }
-    // else some notification of score or total reset
 
-    // removeAnsweredQuestion();
   }
   for (let i = 0; i < countryOptionsElements.length; i++) {
     const countryOptionsElement = countryOptionsElements[i];
@@ -390,58 +394,204 @@ function america() {
 }
 //asia
 function asia() {
-  // let asiaQuestions = [
-  //   { question: 'If you were to visit the land of samba music and Copacabana beach, which country would you go?', answer: 'Brazil' },
-  //   { question: 'You can visit the Statue of Liberty and watch world famous musicals at Broadway, which country should you be?', answer: 'United States' },
-  //   { question: 'You will taste exquisite tequila where it was invented and visit old Mayan pyramides. Which country will you land?', answer: 'Mexico' },
-  //   { question: 'You can visit the cities of Medellin and Cali, most famous for its past drug cartels and feared drug lords. Where will you go', answer: 'Colombia' },
-  //   { question: 'You can dine a tasty barbecue while watching first class tango dancers. Which country should you be?', answer: 'Argentina' },
-  //   { question: 'You can visit the second largest country of the world. A country that has more lakes than the rest of the world combined. Which country will you go?', answer: 'Canada' },
-  //   { question: 'You will be able to visit Machu Pichu. Which country will you go?', answer: 'Peru' },
-  //   { question: 'You can learn some words in Guarany - an official language of this country along with spanish. Where should you go?', answer: 'Paraguay' },
-  //   { question: 'You can stay at an hotel made almost entirely of salt at the edge of the Salar the Uyuni plain. Which country will you go?', answer: 'Bolivia' },
-  //   { question: 'You can drink rum and smoke artisanal cigars at the largest island of the Caribbean. Where will you go?', answer: 'Cuba' },
-  //   { question: 'You can visit the Atacama Desert, the Andes Mountains and Patagonia without living this country. Where would you go?', answer: 'Chile' },
-  //   { question: 'You can visit it\'s capital, Santo Domingo, the oldest European settlement in the Americas founded in 1496. Where will you go?', answer: 'Dominican Republic' },
-  //   { question: 'You can get to know the birthplace of reggae and Bob Marley. Where will you go?', answer: 'Jamaica' },
-  //   { question: 'You will meet people - also known as Ticos - from one of the happiest countries of the world. Where will you go?', answer: 'Costa Rica' },
-  // ];
-  // const asianCountries = ['United States', 'Brazil', 'Mexico', 'Colombia', 'Argentina', 'Canada', 'Peru', 'Venezuela', 'Chile', 'Ecuador', 'Guatemala', 'Cuba', 'Bolivia', 'Haiti', 'Dominican Republic', 'Honduras', 'Paraguay', 'Nicaragua', 'El Salvador', 'Costa Rica', 'Panama', 'Uruguay', 'Jamaica', 'Trinidad and Tobago', 'Guyana', 'Suriname', 'Guadeloupe', 'Martinique', 'Bahamas', 'Belize', 'Barbados'];
+  let asiaQuestions = [
+    { question: 'You can visit the land regarded by the Christian, Jewish and Muslim religious as the Holy Land, which country would you go?', answer: 'Israel' },
+    { question: 'You can visit the famous Forbiden City and The Great Wall, which country should you be?', answer: 'China' },
+    { question: 'You will visit the Taj Mahal and the coutry where the yoga and chess were invented. Which country will you go?', answer: 'India' },
+    { question: 'You can visit a small and predominantly Buddhist country that is also the only country in the world where tobacco is forbidden. Where will you go', answer: 'Bhutan' },
+    { question: 'You can visit more than 2,200 temples and pagodas in the Bagan plains. Which country you will go?', answer: 'Myanmar' },
+    { question: 'You will visit the mythical island of Bali. Which country will you go?', answer: 'Indonesia' },
+    { question: 'In the so-called \'Land of the rising sun\' you can see Mount Fuji. Which country will you go for that?', answer: 'Japan' },
+    { question: 'You will visit some of its 1190 low-lying coral islands located amid the Indian Ocean. Where will you go?', answer: 'Maldives' },
+    { question: 'You would get to know the birthplace of the feared conquerer Genghis Khan. Which country will you go?', answer: 'Mongolia' },
+    { question: 'In this country you can visit the Himalaian Mountains and greet other people saying Namaste. Where will you go?', answer: 'Nepal' },
+    { question: 'You can visit the largest country in the world and drink some vodka. Where would you go?', answer: 'Russia' },
+    { question: 'You can get to know the so-called \'Lion City\', some man-made waterfalls and trees made of fluorescent lights. Where will you go?', answer: 'Singapore' },
+    { question: 'You can listen to some K-Pop, join the crowds that follow e-games and do some serious shopping in Seul. Where will you go?', answer: 'South Korea' },
+    { question: 'You will visit a formet Soviet republic that has 5 UNESCO World Heritage Sites and the shrinking Aral Sea. Where will you go?', answer: 'Uzbekistan' },
+  ];
+  const asianCountries = ['Armenia', 'Bhutan', 'China', 'India', 'Indonesia', 'Israel', 'Japan', 'Jordan', 'Laos', 'Malaysia', 'Maldives', 'Mongolia', 'Myanmar', 'Nepal', 'Philippines', 'Russia', 'Singapore', 'South Korea', 'Thailand', 'Sri Lanka', 'Vietnam', 'Nepal', 'Uzbekistan', 'Kyrgyzstan', 'Bangladesh', 'Syria', 'Qatar', 'Tajikistan', 'Lebanon', 'Bahrain', 'Pakistan'];
+  let randomQuestionIndex = 0;
+  let currentQuestion = {};
+  let possibleAnswers = [];
+  let round = 1;
+  let score = 0;
 
-}
-//africa
-function africa() {
-  console.log('Africa')
-}
-//oceania
-function oceania() {
-  console.log('Oceania');
-
-}
-
-// helper functions
-
-function randomIntergerUpto(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-// Fisher-Yates (aka Knuth) Shuffle. Taken from
-// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array 
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+  function stylePage() {
+    for (let i = 0; i < countryOptionsElements.length; i++) {
+      answersContainer.appendChild(countryOptionsElements[i]);
+    }
+    countDisplay.style.backgroundColor = 'rgba(255,255,255, 0.8)';
+    countDisplay.style.color = 'black';
+    headerTitle.innerHTML = 'Asia';
+    questionToGuessElement.style.height = '60px';
+    questionToGuessElement.style.padding = '20px';
+    for (let i = 0; i < continentOptions.length; i++) {
+      continentContainer.removeChild(continentOptions[i]);
+    }
   }
 
-  return array;
+  function populateAnswers() {
+    unshuffledPossibleAnswers = [];
+    console.log(currentQuestion.answer);
+
+    unshuffledPossibleAnswers.push(currentQuestion.answer);
+
+    while (unshuffledPossibleAnswers.length < 4) {
+      const randomCountry = asianCountries[randomIntergerUpto(asianCountries.length - 1)];
+      let isAlreadyInArray = false;
+
+      for (let i = 0; i < unshuffledPossibleAnswers.length; i++) {
+        if (randomCountry === unshuffledPossibleAnswers[i]) isAlreadyInArray = true;
+      }
+
+      if (!isAlreadyInArray) unshuffledPossibleAnswers.push(randomCountry);
+    }
+
+    possibleAnswers = shuffle(unshuffledPossibleAnswers)
+  }
+
+  function startRound() {
+    // get one random question from europe questions by splicing it out of the Array. 
+    // splice random one:
+    randomQuestionIndex = randomIntergerUpto(asiaQuestions.length - 1);
+    currentQuestion = asiaQuestions.splice(randomQuestionIndex, 1)[0];
+
+    populateAnswers();
+
+    const selectedQuestion = currentQuestion.question;
+    //display selected question
+    questionToGuessElement.innerHTML = selectedQuestion;
+    //display random country's names in the boxes
+    for (let i = 0; i < countryOptionsElements.length; i++) {
+      const country = possibleAnswers[i];
+      const countryOptionsElement = countryOptionsElements[i];
+      countryOptionsElement.innerHTML = country;
+    }
+
+  }
+  //check if clicked element corresponds to the right answer
+  function answerClick(event) {
+    const button = event.target;
+    const clickedIndex = Number(button.getAttribute('data-index'));
+    console.log(clickedIndex);
+
+    if (possibleAnswers[clickedIndex] === currentQuestion.answer) {
+      evaluateRightAnswer.innerHTML = 'Cooool. You have conquered it!';
+      score += 1;
+      rightAnswerAudio.play();
+    } else {
+      evaluateRightAnswer.innerHTML = 'Not this time :(';
+      wrongAnswerAudio.play();
+    }
+
+    evaluateRightAnswer.style.backgroundColor = 'rgba(255,255,255, 0.7)';
+    round += 1
+    // if round < 10
+    if (round < 11) {
+      setTimeout(newQuestion, 2000);
+      setTimeout(function () {
+        questionNumber.innerHTML = `${round}`;
+      }, 2000);
+      console.log(`the score is ${score}`);
+    } else {
+      setTimeout(rank, 2000);
+      setTimeout(clearPage, 2000);
+
+    }
+
+  }
+  for (let i = 0; i < countryOptionsElements.length; i++) {
+    const countryOptionsElement = countryOptionsElements[i];
+    countryOptionsElement.addEventListener('click',
+      answerClick
+    );
+  }
+
+  //Restart game next question
+  function newQuestion() {
+    //remove selected question
+    questionToGuessElement.innerHTML = '';
+    //remove feedback box
+    evaluateRightAnswer.innerHTML = '';
+    evaluateRightAnswer.style.backgroundColor = 'rgba(255,255,255, 0.1)';
+    // //remove countries from boxes
+    for (let i = 0; i < countryOptionsElements.length; i++) {
+      countryOptionsElements[i].innerHTML = '';
+    }
+    startRound();
+  }
+
+  //rank player
+  function rank() {
+
+    if (score < 4) {
+      evaluateRightAnswer.innerHTML = `You\'ve got only ${score} right answers out of 10 😢 </br> Go on and get that old backpack for a ride. 🎒 </br> You deserve some travelling!😎`
+    } else if (score < 7) {
+      evaluateRightAnswer.innerHTML = `You\'ve got ${score} right answers out of 10. </br> Good on you! 🙌 We\'ve seen that you\'ve been around!! 😉`
+    } else {
+      evaluateRightAnswer.innerHTML = `You\'ve got ${score} right answers out of 10. </br> WOW! 🤩 You have been doing some serious traveling 🛫`
+    }
+  }
+  function clearPage() {
+    for (let i = 0; i < countryOptionsElements.length; i++) {
+      answersContainer.removeChild(countryOptionsElements[i]);
+    }
+    document.querySelector('.section').removeChild(questionToGuessElement);
+    evaluateRightAnswer.style.height = '120px';
+    evaluateRightAnswer.style.padding = '30px';
+    evaluateRightAnswer.style.animation = 'color-change 3s linear infinite alternate';
+    evaluateRightAnswer.style.fontSize = '25px';
+    countDisplay.style.backgroundColor = 'rgba(255,255,255, 0.1)';
+    countDisplay.style.color = 'rgba(255,255,255, 0.1)';
+    holidayAudio.play();
+  };
+  stylePage();
+  startRound();
 }
+
+
+//africa
+function africa() {
+  console.log('africa');
+
+  //   // let africaQuestions = [
+  //   //   { question: 'You can climb up the Mount Kilimanjaro, which country would you go?', answer: 'Tanzania' },
+  //   //   { question: 'You can visit the Table Mountain and overlook the Cape Town Bay, which country should you be?', answer: 'South Africa' },
+  //   //   { question: 'You will visit the world\'s most famous pyramides. Which country will you go?', answer: 'Egypt' },
+
+  //   // const africanCountries = ['Algeria', 'Angola', 'Benin', 'Botswana', 'Cabo Verde', 'Cameroon', 'Cote d\'Ivoire', 'Egypt', 'Equatorial Guinea', 'Ethiopia', 'Gabon', 'Ghana', 'Guinea', 'Kenya', 'Madagascar', 'Malawi', 'Mauritius', 'Morocco', 'Mozambique', 'Nigeria', 'Rwanda', 'Senegal', 'South Africa', 'Tanzania', 'Uganda', 'Zambia', 'Zimbabwe', 'Togo', 'Seychelles', 'Somalia', 'Chad'];
+
+  }
+  //oceania
+  function oceania() {
+    console.log('Oceania');
+
+  }
+
+  // helper functions
+
+  function randomIntergerUpto(max) {
+    return Math.floor(Math.random() * (max + 1));
+  }
+
+  // Fisher-Yates (aka Knuth) Shuffle. Taken from
+  // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array 
+  function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
 
